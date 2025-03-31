cac2e75ba3432d3650e5c969e25e7d37a6b3f35e
luccky0 <128980914+luccky0@users.noreply.github.com>
2025-03-31 02:19:08 +0200

    bonne change

--- app/src/main/java/com/example/appli_mobile/LoginActivity.java
+++ app/src/main/java/com/example/appli_mobile/LoginActivity.java
@@ -2,12 +2,19 @@ package com.example.appli_mobile;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import com.example.appli_mobile.utils.SessionManager;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 // activité de login
 public class LoginActivity extends Activity {
@@ -32,16 +39,42 @@ public class LoginActivity extends Activity {
             public void onClick(View view) {
                 String username = etUsername.getText().toString().trim();
                 String password = etPassword.getText().toString().trim();
+                String urlString = "http://192.168.1.132/powerhome/login.php?email=" + username + "&password=" + password;
 
-                // vérification simple entrer user / pass
-                if (username.equals("user") && password.equals("pass")) {
-                    SessionManager session = new SessionManager(LoginActivity.this);
-                    session.setLogin(true);
-                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                    finish();
-                } else {
-                    Toast.makeText(LoginActivity.this, "identifiants incorrects", Toast.LENGTH_SHORT).show();
-                }
+                Ion.with(LoginActivity.this)
+                        .load(urlString)
+                        .asString()
+                        .setCallback(new FutureCallback<String>() {
+                            @Override
+                            public void onCompleted(Exception e, String result) {
+                                if (e != null) {
+                                    Toast.makeText(LoginActivity.this, "Erreur de connexion", Toast.LENGTH_LONG).show();
+                                    return;
+                                }
+                                try {
+                                    JSONObject jsonObject = new JSONObject(result);
+                                    if (jsonObject.has("token")) {
+                                        String token = jsonObject.getString("token");
+                                        String expiration = jsonObject.getString("expired_at");
+
+                                        Toast.makeText(LoginActivity.this, "Connexion réussie!", Toast.LENGTH_SHORT).show();
+
+                                        SharedPreferences sharedPreferences = getSharedPreferences("user_prefs", MODE_PRIVATE);
+                                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                                        editor.putString("token", token); // Stocke le token reçu du serveur
+                                        editor.putString("userEmail", username); // email est récupéré lors de la connexion
+                                        editor.apply();
+                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, "Erreur: " + result, Toast.LENGTH_LONG).show();
+                                    }
+                                } catch (JSONException ex) {
+                                    Toast.makeText(LoginActivity.this, "Mot de passe ou mail incorrect", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
             }
         });
 
--- app/src/main/java/com/example/appli_mobile/RegisterActivity.java
+++ app/src/main/java/com/example/appli_mobile/RegisterActivity.java
@@ -3,11 +3,17 @@ package com.example.appli_mobile;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import com.example.appli_mobile.utils.SessionManager;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 // activité d'inscription
 public class RegisterActivity extends Activity {
@@ -36,17 +42,43 @@ public class RegisterActivity extends Activity {
                 String confirm = etConfirm.getText().toString().trim();
 
                 if (!password.equals(confirm)) {
-                    Toast.makeText(RegisterActivity.this, "les mots de passe ne correspondent pas", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(RegisterActivity.this, "Les mots de passe ne correspondent pas", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                // inscription simulée
-                Toast.makeText(RegisterActivity.this, "inscription réussie", Toast.LENGTH_SHORT).show();
-                // auto-login après inscription
-                SessionManager session = new SessionManager(RegisterActivity.this);
-                session.setLogin(true);
-                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
-                finish();
+                String urlString = "http://192.168.1.132/powerhome/register.php?firstname=" + username  + "&lastname=" + null
+                        + "&email=" + email
+                        + "&password=" + password;
+
+                Ion.with(RegisterActivity.this)
+                        .load(urlString)
+                        .asString()
+                        .setCallback(new FutureCallback<String>() {
+                            @Override
+                            public void onCompleted(Exception e, String result) {
+                                if (e != null) {
+                                    Toast.makeText(RegisterActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+
+                                try {
+                                    JSONObject jsonObject = new JSONObject(result);
+                                    boolean estEnregistrer = jsonObject.getBoolean("success");
+
+                                    if (estEnregistrer) {
+                                        //redirection vers login
+                                        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    } else {
+                                        String message = jsonObject.getString("message");
+                                        Toast.makeText(RegisterActivity.this, message, Toast.LENGTH_SHORT).show();
+                                    }
+                                } catch (JSONException ex) {
+                                    Toast.makeText(RegisterActivity.this, "Erreur JSON", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
             }
         });
     }
